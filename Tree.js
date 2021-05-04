4f15552d4242e8db689ca2b139f8c91e113e58b2
Batool-Alghamdi <batool.alghamdi@tuwaiq.edu.sa>
2021-05-05 01:49:31 +0300

    step 1 updated

--- Tree.js
+++ Tree.js
@@ -4,10 +4,12 @@ let x = canvas.width / 2;
 let y = 40;
 let lineLength = 50;
 let angel = 30;
-let radius = 30;
+let radius = 20;
 let incX = 8.5;
 let incY = 7.3;
-let counter = 0;
+let gap = 200;
+let counter = 1;
+let maxChild = 0;
 ctx.strokeStyle = "black";
 ctx.font = "10px sans-serif";
 let root = document.getElementsByTagName("body")[0];
@@ -18,30 +20,16 @@ node = {
   parent: null,
   depth: 0,
 };
-let NodeTree = [];
-//  (root) => {
-//   for (let i = 0; i < root.childNodes.length; i++) {
-//     if (root.childNodes.item(i).nodeName !== "#text")
-//       node.children.push(NodeTree(root.childNodes.item(i)));
-//   }
-// };
 const addNode = (node) => {
   NodeTree.push(node);
 };
-// for (let i = 0; i < root.childNodes.length; i++) {
-//   if (root.childNodes.item(i).nodeName !== "#text") {
-//     node.children.push(root.childNodes.item(i));
-//   }
-// }
-//console.log(NodeTree(root));
-//NodeTree.push(node);
 ctx.beginPath();
 ctx.arc(node.position.x, node.position.y, radius, 0, Math.PI * 2);
 ctx.stroke();
 drawTree(node);
 function drawTree(tree) {
   let axisY = tree.position.y + 100;
-  let axisX = x - radius * tree.children.length;
+  let axisX = tree.position.x - radius * (maxChild / counter);
   //x -= radius * tree.children.length;
   for (let i = 0; i < tree.children.length; i++) {
     ctx.beginPath();
@@ -52,17 +40,21 @@ function drawTree(tree) {
     ctx.lineTo(tree.position.x, tree.position.y);
     ctx.stroke();
     if (tree.children[i].tag.hasChildNodes) {
+      counter++;
       drawTree(tree.children[i]);
     } else {
       console.log(`No ${tree.children[i].tag.nodeName}`);
     }
-    axisX += 70;
+    axisX += 100;
   }
 }
 console.log(createTree(root));
 function createTree(rootNode) {
   let nodes = [];
   if (rootNode.hasChildNodes) {
+    if (rootNode.childNodes.length > maxChild) {
+      maxChild = rootNode.childNodes.length;
+    }
     for (let i = 0; i < rootNode.childNodes.length; i++) {
       if (rootNode.childNodes.item(i).nodeName !== "#text") {
         let currentNode = {
@@ -73,7 +65,6 @@ function createTree(rootNode) {
           children: createTree(rootNode.childNodes.item(i)),
         };
         nodes.push(currentNode);
-        counter++;
       }
     }
   }
